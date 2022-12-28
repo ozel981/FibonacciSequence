ff631967bb9ddfedb08a77a1a8f9f38ffd7cdde5
Wojciech Podmokły <wpodmokly@gmail.com>
2022-12-28 11:28:07 +0100

    dockerfile

--- fibonacciSequence.c
+++ fibonacciSequence.c
@@ -3,48 +3,53 @@
 
 #define LENGTH 60
 
-unsigned long long* sequence;
+unsigned long long *sequence;
 
 unsigned long long GetFibVal(int number)
 {
     switch (number)
     {
-    case 1: return sequence[number - 1];
-    case 2: return sequence[number - 1];
+    case 1:
+        return sequence[number - 1];
+    case 2:
+        return sequence[number - 1];
     default:
     {
-        if (sequence[number - 1] != 0) return sequence[number - 1];
-        else return (sequence[number - 1] = GetFibVal(number - 2) + GetFibVal(number - 1));
+        if (sequence[number - 1] != 0)
+            return sequence[number - 1];
+        else
+            return (sequence[number - 1] = GetFibVal(number - 2) + GetFibVal(number - 1));
     }
     }
 }
 
 int main()
 {
-    sequence = (unsigned long long*) malloc(LENGTH * sizeof(unsigned long long));
-    for (int i = 0; i < LENGTH; ++i) {
+    sequence = (unsigned long long *)malloc(LENGTH * sizeof(unsigned long long));
+    for (int i = 0; i < LENGTH; ++i)
+    {
         sequence[i] = 0;
     }
     sequence[1] = 1;
 
-    if (sequence == NULL) {
-        printf("Error! memory not allocated.");
+    if (sequence == NULL)
+    {
+        printf("Error! memory not allocated.\n");
         exit(0);
     }
     int number;
 
-    printf("Get sequence number [1,%d]:", LENGTH);
     scanf("%d", &number);
 
     if (number < 1 || number > LENGTH)
     {
-        printf("Error! Incorrect sequence number [1,%d]", LENGTH);
+        printf("Error! Incorrect sequence number [1,%d]\n", LENGTH);
         exit(0);
     }
 
     printf("%llu \n", GetFibVal(number));
     printf("fibonacciSequence \n");
-    printf("Wojciech Podmok³y \n");
+    printf("Wojciech Podmokly \n");
     printf("Grupa dziekanska 2.3");
 
     free(sequence);
